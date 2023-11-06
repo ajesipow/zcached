f8cf0aa27d696a5d697bccaa3827a8f04c71c149
ajesipow <alexander.jesipow@gmail.com>
2023-11-06 19:31:44 +0100

    Refactor benches

--- zcached/benches/server.rs
+++ zcached/benches/server.rs
@@ -23,17 +23,22 @@ fn get_db_key(c: &mut Criterion) {
     let (senders, receivers): (Vec<Sender<()>>, Vec<Receiver<()>>) =
         (0..10).map(|_| channel()).unzip();
 
+    // let (mut rng, keys, values) = get_random_data();
+    // let (data, data_distribution) = get_data_actions_and_distributions(&mut rng, &keys, &values);
+
     // We spawn some threads that can receive work in the actual benchmark.
     // This saves us the overhead of creating new threads for each iteration of the benchmark,
     // which would dominate the measurement.
     for rx in receivers {
         let db_clone = db.clone();
         thread::spawn(move || loop {
+            // We create these strings ahead of time so that the allocation does not contribute to
+            // the benchmark.
+            let key = "hello".to_string();
+            let value = "world".to_string();
             if rx.recv().is_ok() {
-                db_clone
-                    .insert("hello".to_string(), "world".to_string())
-                    .unwrap();
-                db_clone.get("hello").unwrap();
+                db_clone.get(&key).unwrap();
+                db_clone.insert(key, value).unwrap();
             }
         });
     }
@@ -96,6 +101,53 @@ fn random_client_action<'a>(
     };
 }
 
+fn get_random_data() -> (StdRng, Vec<String>, Vec<String>) {
+    let mut rng = StdRng::seed_from_u64(42);
+    let keys: Vec<String> = (0..10_000)
+        .map(|_| {
+            let key_len = rng.gen_range(5..=32);
+            Alphanumeric.sample_string(&mut rng, key_len)
+        })
+        .collect();
+    let values: Vec<String> = (0..10_000)
+        .map(|_| {
+            let value_len = rng.gen_range(32..=256);
+            Alphanumeric.sample_string(&mut rng, value_len)
+        })
+        .collect();
+
+    (rng, keys, values)
+}
+
+fn get_data_actions_and_distributions<'a>(
+    rng: &mut StdRng,
+    keys: &'a [String],
+    values: &'a [String],
+) -> (Vec<RandomAccessClientSetup<'a>>, Uniform<usize>) {
+    let key_dist = Uniform::from(0..keys.len());
+    let value_dist = Uniform::from(0..values.len());
+    let data: Vec<RandomAccessClientSetup> = (0..100_000)
+        .map(|_| match rng.gen::<f64>() {
+            x if x <= 0.4 => {
+                let key = &keys[key_dist.sample(rng)];
+                let value = &values[value_dist.sample(rng)];
+                RandomAccessClientSetup::Set { key, value }
+            }
+            x if 0.4 < x && x <= 0.8 => {
+                let key = &keys[key_dist.sample(rng)];
+                RandomAccessClientSetup::Get(key)
+            }
+            x if 0.8 < x && x <= 0.95 => {
+                let key = &keys[key_dist.sample(rng)];
+                RandomAccessClientSetup::Delete(key)
+            }
+            _ => RandomAccessClientSetup::Flush,
+        })
+        .collect();
+    let distribution = Uniform::from(0..data.len());
+    (data, distribution)
+}
+
 fn set_and_get_random_access(c: &mut Criterion) {
     let host = "127.0.0.1";
     let server = Server::builder()
@@ -111,41 +163,8 @@ fn set_and_get_random_access(c: &mut Criterion) {
 
     let mut client = Client::connect(format!("{host}:{port}"));
 
-    let mut rng = StdRng::seed_from_u64(42);
-    let keys: Vec<String> = (0..10_000)
-        .map(|_| {
-            let key_len = rng.gen_range(5..=32);
-            Alphanumeric.sample_string(&mut rng, key_len)
-        })
-        .collect();
-    let values: Vec<String> = (0..10_000)
-        .map(|_| {
-            let value_len = rng.gen_range(32..=256);
-            Alphanumeric.sample_string(&mut rng, value_len)
-        })
-        .collect();
-    let key_dist = Uniform::from(0..keys.len());
-    let value_dist = Uniform::from(0..values.len());
-    let data: Vec<RandomAccessClientSetup> = (0..100_000)
-        .map(|_| match rng.gen::<f64>() {
-            x if x <= 0.4 => {
-                let key = &keys[key_dist.sample(&mut rng)];
-                let value = &values[value_dist.sample(&mut rng)];
-                RandomAccessClientSetup::Set { key, value }
-            }
-            x if 0.4 < x && x <= 0.8 => {
-                let key = &keys[key_dist.sample(&mut rng)];
-                RandomAccessClientSetup::Get(key)
-            }
-            x if 0.8 < x && x <= 0.95 => {
-                let key = &keys[key_dist.sample(&mut rng)];
-                RandomAccessClientSetup::Delete(key)
-            }
-            _ => RandomAccessClientSetup::Flush,
-        })
-        .collect();
-
-    let data_distribution = Uniform::from(0..data.len());
+    let (mut rng, keys, values) = get_random_data();
+    let (data, data_distribution) = get_data_actions_and_distributions(&mut rng, &keys, &values);
 
     c.bench_function("set_and_get_random_access", |b| {
         b.iter(|| random_client_action(&mut client, &data, &data_distribution, &mut rng))
